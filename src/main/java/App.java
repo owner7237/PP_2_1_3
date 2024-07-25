fb8216ed9d1717bf19437d5e4673384cdf33c5ae
owner7237 <132247769+owner7237@users.noreply.github.com>
2024-07-25 23:13:27 +0300

    init

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,21 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.SortedMap;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(bean == bean2);
+        System.out.println(cat == cat2);
     }
 }
